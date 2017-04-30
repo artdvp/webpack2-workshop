f37970e1d37b5c0c55604ef64674c1cccb2e3247
artdvp <sphinspf@gmail.com>
2017-04-30 15:37:52 +0000

    install css-loader style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,15 @@ module.exports = {
   entry: './src/js/app.js',
   output: {
       path: path.resovle(__dirname, 'dist'),
-      filename: 'bundle.js'
+      filename: 'bundle.js',
+      publicPath: '/dist'
+  },
+  module: {
+      rules: [
+        {
+            test: /\.css$/,
+            loader: 'css-loader'
+        }
+      ]
   }
 };
