8e34aed0621dbe8494eae7eaf6d0c51ba331b8a5
artdvp <sphinspf@gmail.com>
2017-04-30 16:56:49 +0000

    edit up onfig webpack2

--- webpack2-basic2-babel-scss/webpack.config.js
+++ webpack2-basic2-babel-scss/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                 test: /\.scss$/,
                 use: extractPlugin.extract({
                    use: ['css-loader','sass-loader'] 
-                });
+                })
             }
         ]
     },
