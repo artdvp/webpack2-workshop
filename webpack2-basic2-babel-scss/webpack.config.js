b59ac30910c390b1f2a768b83c2e748b36e4b6ff
artdvp <sphinspf@gmail.com>
2017-05-01 01:14:17 +0700

    update webpack.config.js

--- webpack2-basic2-babel-scss/webpack.config.js
+++ webpack2-basic2-babel-scss/webpack.config.js
@@ -1,9 +1,10 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
-   filename: 'main.css' 
+   filename: 'main.css'
 });
 
 module.exports = {
@@ -29,7 +30,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: extractPlugin.extract({
-                   use: ['css-loader','sass-loader'] 
+                   use: ['css-loader','sass-loader']
                 })
             },
             {
@@ -43,8 +44,8 @@ module.exports = {
                         loader: 'file-loader',
                         options: {
                             name: '[name].[ext]',
-                            outputPath: 'img/',
-                            publicPath: 'img/'
+                            outputPath: 'img/'
+                            //publicPath: 'img/'
                         }
                     }
                 ]
@@ -55,6 +56,9 @@ module.exports = {
         extractPlugin,
         new HtmlWebpackPlugin({
             template: 'src/index.html'
-        })
+        }),
+        new CleanWebpackPlugin([
+          'dist'
+        ])
     ]
 };
