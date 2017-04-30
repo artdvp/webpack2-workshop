98d02f04f13e72408c0e647548fe843aab4c341c
artdvp <sphinspf@gmail.com>
2017-04-30 15:40:09 +0000

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './src/js/app.js',
   output: {
-      path: path.resovle(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
       publicPath: '/dist'
   },
